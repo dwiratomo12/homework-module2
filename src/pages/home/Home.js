8b14c53833c5750797a0facb458ce58e6968fc50
dwiratomo12 <dwiratomo@gmail.com>
2022-03-28 11:27:23 +0700

    menambahkan looping pada data

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,25 +2,38 @@ import React, { useState, useEffect } from "react";
 import data from "../../Data";
 
 function Home() {
-  const artists = data[0].album.artists[0].name;
-  const title = data[0].album.name;
-  const gambar = data[0].album.images[1].url;
-  const width = data[0].album.images[1].width;
-  const height = data[0].album.images[1].height;
-  const img = <img src={gambar} width={width} height={height}></img>;
-  console.log(data);
+  const Card = {
+    margin: "10px",
+    border: "1px solid #000",
+    padding: "10px",
+    width: "200px",
+    borderRadius: "20px",
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+  };
+
+  const Button = {
+    paddingTop: "5px",
+    paddingBottom: "5px",
+    width: "70px",
+    color: "white",
+    backgroundColor: "blue",
+  };
+
   return (
-    <div>
-      My Music <br />
-      {img}
-      {/* <img src={data.album.images[1].url} width={data.album.images[1].width} height={data.album.images[1].height}></img> */}
-      <br />
-      <br />
-      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
-      <h3>{title}</h3>
-      <p>{artists}</p>
-      <button type="submit">Select</button>
-    </div>
+    <>
+      <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap", backgroundColor: "rgb(149, 223, 232)" }}>
+        {data.map((d) => {
+          return (
+            <div style={Card}>
+              <img src={d.album.images[1].url} width="200" style={{ borderRadius: "20px" }}></img>
+              <h3>{d.name}</h3>
+              <p>{d.artists[0].name}</p>
+              <button style={Button}>Select</button>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
